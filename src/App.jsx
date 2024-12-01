7211c5d41a15e2b2ee832aded1f048f709601328
27Adityasahil <adityasahil24@gmail.com>
2024-12-01 13:47:13 +0530

    organize App

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,63 @@ import MemberCard from "./components/MemberCard";
 import Contact from "./components/Contact";
 import Team from "./components/Team";
 import { BrowserRouter, Router, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
       <ImageSlider />
-      <EventCard
-        data={{
-          eventName: "Cyberverse 2025",
-          eventImage:
-            "https://images.pexels.com/photos/28870747/pexels-photo-28870747/free-photo-of-cinematic-urban-portrait-in-new-york-city.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-          eventDate: "01 Jan 2025",
-          eventTag: "annual-event",
-          eventDesc:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem debitis aperiam dicta tempore aspernatur exercitationem quae? Optio voluptas ipsa repellendus placeat commodi rem molestias ",
-        }}
-      />
-      
-      
+      <hr />
+      <div className="event-section">
+        <h3 className="event-sec-head">Events</h3>
+        <p className="upcoming-events">Upcoming Events</p>
+        <EventCard
+          data={{
+            eventName: "Cyberverse 2025",
+            eventImage:
+              "https://images.pexels.com/photos/28870747/pexels-photo-28870747/free-photo-of-cinematic-urban-portrait-in-new-york-city.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+            eventDate: "01 Jan 2025",
+            eventTag: "annual-event",
+            eventDesc:
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem debitis aperiam dicta tempore aspernatur exercitationem quae? Optio voluptas ipsa repellendus placeat commodi rem molestias ",
+          }}
+        />
+      </div>
+      <hr />
+      <div className="why-cybercell">
+        <h3 className="why-cybercell-head">Why Cybercell?</h3>
+        <p className="why-cybercell-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          corporis maxime mollitia hic tenetur sit totam ut exercitationem, eum
+          laudantium optio earum fugit? Beatae nesciunt nisi iure excepturi eum
+          totam dolorem molestiae! Labore earum sapiente sunt, officiis optio,
+          obcaecati suscipit nulla minus laborum eos nemo distinctio provident
+          non repellat quia libero veniam sint inventore tempora est
+          consequuntur? Quae magni esse possimus, quibusdam molestias eum
+          consequatur earum, natus nulla magnam quasi tenetur deleniti sequi
+          eius exercitationem! Similique excepturi enim eveniet iste quisquam
+          commodi dolor explicabo ullam, voluptatem ab consectetur, expedita,
+          assumenda corrupti doloribus sit unde voluptatibus laborum voluptate
+          ex velit minus impedit. Quae dignissimos numquam adipisci libero odit
+          unde ullam voluptatem nemo placeat quos incidunt magni, deserunt amet
+          dolores quaerat, nihil veniam dicta! Nam vitae eos autem fuga
+          temporibus recusandae accusamus corporis ab odio doloremque non
+          dolores, architecto animi minima nisi nihil, harum quaerat quos
+          blanditiis cupiditate ipsa. Voluptatum, ipsum nostrum!
+        </p>
+      </div>
+
+      <hr />
+
       <TestimonialCard />
+      <hr />
       <FooterSection />
-      <EventPoster />
+      {/* <EventPoster />
       <About />
       <MemberCard />
       <Contact />
-      <Team />
+      <Team /> */}
     </React.Fragment>
   );
 };
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import "./TestimonialCard.css";
 
 const TestimonialCard = () => {
-  return <>
-    
-    <div className="testimonial-box"><h3 className="sub-heading">Testimonials</h3>
-    <div className="testimonial-container">
-      <div className="message-box">
-        <p className="echo">$echo</p>
-        <p className="message">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente molestiae deserunt alias rerum explicabo accusantium velit quae. Explicabo reiciendis, quis nesciunt excepturi veritatis, unde incidunt necessitatibus iste nam illum eos, praesentium accusamus! Maiores ratione atque reiciendis in deserunt cum aliquid velit? Deleniti, repellat quam facere sed accusamus laborum nulla amet?</p>
-          <p className="user-name">Name</p>
-          <p className="user-name">Member</p>
-          <p className="user-name">(year)</p>
+  return (
+    <>
+      <div className="testimonial-box">
+        <h3 className="sub-heading">Testimonials</h3>
+        <div className="testimonials-msgs">
+       
+        <div className="testimonial-card">
+          <div className="img-circle">
+          <img src="https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg" alt="user-image" />
+         
+          <p className="user-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident obcaecati cum saepe, expedita quas aliquam debitis autem deleniti possimus mollitia repellendus aut praesentium molestiae libero sunt nulla eaque dolore minus! Ullam hic facilis dicta dolorem in rem autem omnis explicabo?</p>
+          </div>
+        </div>
         
+        </div>
       </div>
-      <div className="testimonialImage">
-        <img src="https://images.pexels.com/photos/2787341/pexels-photo-2787341.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="User-photo" />
-      </div>
-    </div>
-    </div>
-  </>
-  
+    </>
+  );
 };
 
 export default TestimonialCard;
